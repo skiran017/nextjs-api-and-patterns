da5eb08a29a9572641c4aabef3d6b29491f10477
skiran017 <skiran017@gmail.com>
2022-05-30 02:21:43 +0530

    create a page and navigate btw them using Link tag
    -The link tag performs client side navigation
    - a tag performs full page reload

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+
+function About() {
+  return (
+    <>
+      <h1>About</h1>
+      <Link href="/">Homepage</Link>
+    </>
+  );
+}
+
+export default About;
